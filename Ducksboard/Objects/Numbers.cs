52ba181ea459b50d9246f615fa16574a2be33956
Khalid Abuhakmeh <khalid@aquabirdconsulting.com>
2013-01-11 10:34:43 -0500

    bumped to version 1.0.1 and pushed to Nuget.

--- Ducksboard/Objects/Numbers.cs
+++ Ducksboard/Objects/Numbers.cs
@@ -44,7 +44,7 @@ namespace Ducksboard.Objects
 
         public static DateTime ConvertFromUnixTimestamp(double timestamp)
         {
-            var origin = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            var origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
             return origin.AddSeconds(timestamp);
         }
 
