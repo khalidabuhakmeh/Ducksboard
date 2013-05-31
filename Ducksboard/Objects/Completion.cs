1421605733dbdaf23e30018401ff0a5c949cabc5
Xavier Poinas <xavier.poinas@gmail.com>
2013-05-31 10:17:20 +1000

    Added support for 'Completion' widget

---
+++ Ducksboard/Objects/Completion.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.Serialization;
+
+namespace Ducksboard.Objects
+{
+    [DataContract]
+    public class Completion : DucksboardObjectBase
+    {
+        public Completion()
+        {
+            Value = new Payload();
+        }
+
+        [DataMember(Name = "value")]
+        public Payload Value { get; set; }
+
+        [DataContract]
+        public class Payload
+        {
+            [DataMember(Name = "current", EmitDefaultValue = false)]
+            public decimal? Current { get; set; }
+
+            [DataMember(Name = "min", EmitDefaultValue = false)]
+            public decimal? Min { get; set; }
+
+            [DataMember(Name = "max", EmitDefaultValue = false)]
+            public decimal? Max { get; set; }
+        }
+    }
+}
